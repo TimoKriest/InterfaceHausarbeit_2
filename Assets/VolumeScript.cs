1126739d0771115925eee6a29e37f707faf7c653
Timo <timokriest@hotmail.de>
2023-01-21 20:26:11 +0100

    Feat: Volume Slider
    Volume slider nun Funktional

---
+++ Assets/VolumeScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeScript : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+
+    private void Start(){
+        if (PlayerPrefs.HasKey("volume")){
+            load();
+            PlayerPrefs.SetFloat("volume", 1);
+        }
+        else{
+            save();
+        }
+    }
+
+   public void ChangeVolume(){
+         AudioListener.volume = volumeSlider.value;
+         save();
+
+   }
+
+   private void load(){
+         volumeSlider.value = PlayerPrefs.GetFloat("volume");
+   }
+
+   private void save(){
+         PlayerPrefs.SetFloat("volume", volumeSlider.value);
+   }
+
+
+}
